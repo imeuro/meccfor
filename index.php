db70150af8e26060ebea19a4044c7bfb99abe362
meuro <mauro.fioravanzi@mondadori.it>
2021-09-09 17:20:55 +0200

    first commit - hp mobile

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+    <meta name="Description" CONTENT="Meccfor è un’azienda esperta, dinamica e professionale che opera nel settore della fornitura di componenti e prodotti finiti oltre che nella costruzione di parti a disegno.">
+
+    <meta name=”robots” content="index, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="canonical" href="https://www.meccfor.com"/>
+	<meta property="og:title" content="Home | Meccfor"/>
+	<meta property="og:url" content="https://www.meccfor.com"/>
+	<meta property="og:site_name" content="Mecfortest"/>
+	<meta property="og:type" content="website"/>
+	<link rel="alternate" href="https://www.meccfor.com/" hreflang="x-default"/>
+	<link rel="alternate" href="https://www.meccfor.com/?lang=en" hreflang="en-gb"/>
+	<link rel="alternate" href="https://www.meccfor.com/" hreflang="it-it"/>
+	<meta name="twitter:card" content="summary_large_image"/>
+	<meta name="twitter:title" content="Home | Meccfor"/>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
+	<link rel="manifest" href="/assets/icons/site.webmanifest">
+	<link rel="mask-icon" href="/assets/icons/safari-pinned-tab.svg" color="#5486a7">
+	<link rel="shortcut icon" href="/assets/icons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#fff6ef">
+	<meta name="msapplication-config" content="/assets/icons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+
+	<title>MECCFOR - Design & Forniture Componenti Meccanici</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/mc.css">
+</head>
+<body>
+
+	<div class="mc-container">
+		<header class="mc-header">
+
+			<nav role="navigation">
+				<div id="menuToggle">
+					<input type="checkbox" />
+					<span></span>
+					<span></span>
+					<span></span>
+
+					<ul id="menu">
+						<li><a href="#" title="Chi siamo">Chi siamo</a></li>
+						<li><a href="#" title="Servizi">Servizi</a></li>
+						<li><a href="#" title="Forniture">Forniture</a></li>
+						<li><a href="#" title="Download">Download</a></li>
+						<li><a href="#" title="Contatti">Contatti</a></li>
+					</ul>
+				</div>
+			</nav>
+			<div class="mc-heading">
+				<h1><img src="assets/graphics/logo-full.svg" alt="MECCFOR - Design & Forniture Componenti Meccanici" class="mc-logo-full"></h1>
+			</div>
+
+		</header>
+
+
+		<div class="mc-content mc-content-homepage">
+			<img  alt="A baby smiling with a yellow headband." srcset="
+			./assets/graphics/meccfor-cover-home-low.jpg 768w,
+			./assets/graphics/meccfor-cover-home.jpg 1024w
+			"
+			sizes="100vw">
+
+			<div class="mc-bigsymbol">
+				<h2 class="mc-tagline">Design & Forniture Componenti Meccanici</h2>
+				<ul id="home-menu" class="mc-menu">
+					<li><a href="#" title="Chi siamo">Chi siamo</a></li>
+					<li><a href="#" title="Servizi">Servizi</a></li>
+					<li><a href="#" title="Forniture">Forniture</a></li>
+					<li><a href="#" title="Download">Download</a></li>
+					<li><a href="#" title="Contatti">Contatti</a></li>
+				</ul>
+			</div>
+		</div>
+
+
+	</div>
+
+	<script src="assets/js/mc.js"></script>
+</body>
+</html>
