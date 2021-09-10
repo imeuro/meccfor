0c06a703b6e8a7b9c18cabc591e0c44115cf91d3
imeuro <mauro.fioravanzi@gmail.com>
2021-09-10 17:52:39 +0200

    fix menu, cover + sottopagine + varie

---
+++ servizi-in-loco.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+    <meta name="Description" CONTENT="Meccfor è un’azienda esperta, dinamica e professionale che opera nel settore della fornitura di componenti e prodotti finiti oltre che nella costruzione di parti a disegno.">
+
+    <meta name=”robots” content="index, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="canonical" href="https://www.meccfor.com"/>
+	<meta property="og:title" content="Home | Meccfor"/>
+	<meta property="og:url" content="https://www.meccfor.com"/>
+	<meta property="og:site_name" content="Mecfortest"/>
+	<meta property="og:type" content="website"/>
+	<link rel="alternate" href="https://www.meccfor.com/" hreflang="x-default"/>
+	<link rel="alternate" href="https://www.meccfor.com/?lang=en" hreflang="en-gb"/>
+	<link rel="alternate" href="https://www.meccfor.com/" hreflang="it-it"/>
+	<meta name="twitter:card" content="summary_large_image"/>
+	<meta name="twitter:title" content="Home | Meccfor"/>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
+	<link rel="manifest" href="/assets/icons/site.webmanifest">
+	<link rel="mask-icon" href="/assets/icons/safari-pinned-tab.svg" color="#5486a7">
+	<link rel="shortcut icon" href="/assets/icons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#fff6ef">
+	<meta name="msapplication-config" content="/assets/icons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+
+	<title>Contatti • MECCFOR - Design & Forniture Componenti Meccanici</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/mc.css">
+</head>
+<body>
+
+	<div class="mc-container">
+		<header class="mc-header">
+
+			<nav role="navigation">
+				<div id="menuToggle">
+					<input type="checkbox" />
+					<span></span>
+					<span></span>
+					<span></span>
+
+					<ul id="menu">
+						<li><a href="chi-siamo" title="Chi siamo">Chi siamo</a></li>
+						<li><a href="servizi-design" title="Servizi">Servizi</a>
+							<ul>
+								<li><a href="servizi-design" title="Design">Design</a></li>
+								<li><a href="servizi-in-loco" title="In Loco">In Loco</a></li>
+							</ul>
+						</li>
+						<li><a href="forniture-riduttori" title="Forniture">Forniture</a>
+							<ul>
+								<li><a href="forniture-riduttori" title="Riduttori">Riduttori</a></li>
+								<li><a href="forniture-altro" title="Altro">Altro</a></li>
+							</ul>
+						</li>
+						<li><a href="download" title="Download">Download</a></li>
+						<li><a href="contatti" title="Contatti">Contatti</a></li>
+					</ul>
+				</div>
+			</nav>
+			<div class="mc-heading">
+				<span><a href="./"><img src="assets/graphics/logo-full.svg" alt="MECCFOR - Design & Forniture Componenti Meccanici" class="mc-logo-full"></a></span>
+			</div>
+
+		</header>
+
+
+		<div class="mc-content">
+			<img width="1920" height="1280" alt="MECCFOR - Servizi" srcset="
+			./assets/graphics/meccfor-cover-servizi-in-loco-low.jpg 768w,
+			./assets/graphics/meccfor-cover-servizi-in-loco.jpg 1920w
+			"
+			sizes="100vw">
+
+			<div class="mc-page-content">
+				<h1 class="mc-page-title">Servizi</h1>
+				<p>Meccfor è un’azienda esperta, dinamica e professionale che opera nel settore della fornitura di componenti e prodotti finiti oltre che nella costruzione di parti a disegno.</p>
+
+				<p>Meccfor è in grado di rispondere al meglio alle richieste dei propri clienti in termini di flessibilità e competenza tecnica.</p>
+
+				<p>Opera prevalentemente nel mercato nazionale garantendo un ottimo rapporto qualità servizio, in grado di soddisfare anche il cliente più esigente e si propone di creare un rapporto di partnership, con l’obiettivo di fornire un prodotto che soddisfi la propria clientela.</p>
+			</div>
+		</div>
+
+
+	</div>
+
+	<script src="assets/js/mc.js"></script>
+</body>
+</html>
