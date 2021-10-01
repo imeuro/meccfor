cb02695b33c7ee5ca4c0f9d29159a49050e25a1d
imeuro <mauro.fioravanzi@gmail.com>
2021-10-01 12:21:54 +0200

    mailobf js error on device vecchietti

--- assets/js/mc.js
+++ assets/js/mc.js
@@ -1,6 +1,6 @@
 /* mail obfuscation */
 var mailitems = document.querySelectorAll(".mc-mailer");
-if (mailitems.length > 0){
+if (mailitems.length){
 	Array.from(mailitems).forEach((el) => {
 		var d = el.dataset;
 		el.href = "mailto:"+d.user+"@"+d.domain;
