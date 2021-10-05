4ac5c0b76156b6eaa74e265adca5081084e6c2cb
imeuro <mauro.fioravanzi@gmail.com>
2021-10-05 11:01:52 +0200

    no webmanifest

--- en/products-gear.php
+++ en/products-gear.php
@@ -10,15 +10,7 @@
 
 	<?php include('inc/meta.php'); ?>
 
-	<link rel="apple-touch-icon" sizes="180x180" href="./assets/graphics/icons/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="./assets/graphics/icons/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="./assets/graphics/icons/favicon-16x16.png">
-	<link rel="manifest" href="./assets/graphics/icons/site.webmanifest">
-	<link rel="mask-icon" href="./assets/graphics/icons/safari-pinned-tab.svg" color="#024189">
-	<link rel="shortcut icon" href="./assets/graphics/icons/favicon.ico">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-config" content="./assets/graphics/icons/browserconfig.xml">
-	<meta name="theme-color" content="#ffffff">
+	<?php include('inc/favicons.php'); ?>
 
 	<title>Products - Gear • MECCFOR - Mechanical Components Supplies</title>
 	<link rel="stylesheet" type="text/css" href="../assets/css/mc.css">
